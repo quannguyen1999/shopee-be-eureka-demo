3d79bd9cae860c320835b53c9bf0064572650d0a
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-04 14:32:27 +0700

    init

---
+++ src/main/java/com/shopee/ecommer/shopeebeeurekademo/ShopeeBeEurekaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shopee.ecommer.shopeebeeurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeBeEurekaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeBeEurekaDemoApplication.class, args);
+	}
+
+}
