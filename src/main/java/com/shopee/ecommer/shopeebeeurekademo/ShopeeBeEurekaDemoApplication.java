866c173465000a40a7f039c22b4a1fe4b8a24b7b
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-04 14:50:48 +0700

    fix bug

--- src/main/java/com/shopee/ecommer/shopeebeeurekademo/ShopeeBeEurekaDemoApplication.java
+++ src/main/java/com/shopee/ecommer/shopeebeeurekademo/ShopeeBeEurekaDemoApplication.java
@@ -2,8 +2,10 @@ package com.shopee.ecommer.shopeebeeurekademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ShopeeBeEurekaDemoApplication {
 
 	public static void main(String[] args) {
